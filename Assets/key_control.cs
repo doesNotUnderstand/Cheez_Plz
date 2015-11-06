803411638b860a791d905676a84e328394e0c22d
shJimmyw <jimmywu@dhcp-v024-170.mobile.uci.edu>
2015-11-05 20:16:30 -0800

    Updated
    Added border to map and reset button

---
+++ Assets/key_control.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class key_control : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown("r"))
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}

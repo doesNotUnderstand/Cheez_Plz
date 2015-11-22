e22c80bf9737d8c44378aae8b81884f513ed1c57
Leon Matsuki <lmatsuki87@gmail.com>
2015-11-22 05:19:19 -0800

    -Drew Cheese Ball, wall colliders for Level 1 -Implemented Cheese Ball, gate, key, cheese for level 1 (Fillable pits, boxes, cats remaining)

---
+++ Assets/_Scripts/Level1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1 : MonoBehaviour {
+
+    public Transform mouse;
+    public cheeseBall cheeseBallScript;
+
+	// Use this for initialization
+	void Start ()
+    {                
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(mouse.GetComponent<playerController>().playerCarryingCheese() && !cheeseBallScript.ballIsMoving())
+        {
+            cheeseBallScript.setMove();
+        }
+	}
+}
---
+++ Assets/_Scripts/cheeseBall.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cheeseBall : MonoBehaviour {
+
+    public Transform target;
+    public playerController playerScript;
+    public float speed;
+
+    bool moving;
+    Transform cheeseBallTransform;
+    Vector2 startingPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cheeseBallTransform = GetComponent<Transform>();
+        startingPosition = cheeseBallTransform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {        
+        MoveToTarget();        
+	}
+
+    public void setMove()
+    {
+        moving = true;
+    }
+
+    public bool ballIsMoving()
+    {
+        return moving;
+    }
+
+    public void resetPosition()
+    {
+        moving = false;
+        cheeseBallTransform.position = startingPosition;
+    }
+
+    void MoveToTarget()
+    {
+        if(moving)
+        {
+            cheeseBallTransform.position = Vector2.MoveTowards(cheeseBallTransform.position, target.position, speed * Time.deltaTime);         
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Mouse")
+        {
+            playerScript.anim.SetBool("MouseFell", true);
+            playerScript.allowMovement(false);
+            playerScript.setPlayerDied(true);
+            resetPosition();
+        }
+    }
+}

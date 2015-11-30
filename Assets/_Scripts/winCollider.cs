a08624f5b929ec7aad48c9a6c10c5c1b516001dd
lmatsuki <lmatsuki87@gmail.com>
2015-11-29 16:15:32 -0800

    Completed score/cheese rating system, updated level 2 background image

--- Assets/_Scripts/winCollider.cs
+++ Assets/_Scripts/winCollider.cs
@@ -9,14 +9,16 @@ public class winCollider : MonoBehaviour {
     public GameObject minimap; // Turn off minimap when level is complete
     public GameObject scoreMenu; // Manage the score display
     public mainCameraScript timerScript; // To stop time when player wins
-    public int[] tutorialRecord = {100, 80, 60 }; // Cheese rating from easy to hard
-    public int[] firstRecord = { 300, 240, 120 };
-    public int[] secondRecord = { 600, 480, 300 };
+    public Sprite cheeseRating; // Image of colored cheese
+    public int[] tutorialRecord = { 60, 80, 100 }; // Cheese rating from hard to easy
+    public int[] firstRecord = { 120, 240, 300 };
+    public int[] secondRecord = { 300, 480, 600 };
     public bool dimLights = false;
     public int menuDelay;
     public int level = 0;    
 
-    private ScoreSystem scoreScript;
+    ScoreSystem scoreScript;
+    int totalLevels = 3; // change this value if more levels are created
     float dimAlpha = 0.0f;
     bool playerInside = false;
     bool cheeseInside = false;
@@ -67,16 +69,25 @@ public class winCollider : MonoBehaviour {
         }
         else if(dimAlpha >= 0.5f)
         {          
+            // Screen is now dark, display level complete, score, and cheese rating
             levelComplete.SetActive(true);
+
+            // Set the time on score
             scoreMenu.GetComponent<Transform>().Find("txt_time").GetComponent<Text>().text 
                 = timerScript.get_levelTimeMinutes() + ":" + timerScript.get_levelTimeSeconds();
-            scoreMenu.SetActive(true);
+            
+            // Update the saved score if necessary
             string levelKey = returnLevelString(level);
             if ((PlayerPrefs.HasKey(levelKey) && PlayerPrefs.GetInt(levelKey) > timerScript.get_levelTime())
                  || !PlayerPrefs.HasKey(levelKey))
             {
                 scoreMenu.GetComponent<Transform>().Find("txt_newhighscore").GetComponent<GameObject>().SetActive(true);
             }
+
+            // Update cheese rating
+            setCheeseImages(returnCheeseRating(level, timerScript.get_levelTime()));
+
+            scoreMenu.SetActive(true);
             scoreShown = true;
         }
     }
@@ -86,6 +97,34 @@ public class winCollider : MonoBehaviour {
     {
         yield return new WaitForSeconds(menuDelay);		
         Application.LoadLevel("Menu");
+    }
+
+    // Change cheese images depending on cheese rating
+    void setCheeseImages(int rating)
+    {
+        if(rating == 3)
+            scoreMenu.GetComponent<Transform>().Find("img_rating3").GetComponent<Image>().sprite = cheeseRating;                
+        if(rating >= 2)
+            scoreMenu.GetComponent<Transform>().Find("img_rating2").GetComponent<Image>().sprite = cheeseRating;                
+        if(rating >= 1)
+            scoreMenu.GetComponent<Transform>().Find("img_rating1").GetComponent<Image>().sprite = cheeseRating;    
+    }
+
+    // Return the cheese rating depending on time cleared
+    int returnCheeseRating(int level, int time)
+    {
+        int[] ratingArray;
+
+        if     (level == 2) ratingArray = secondRecord;
+        else if(level == 1) ratingArray = firstRecord;
+        else                ratingArray = tutorialRecord;        
+
+        for (int i = 0; i < totalLevels; i++)
+        {
+            if (time < ratingArray[i])
+                return 3 - i;
+        }
+        return 0;
     }
 
     string returnLevelString(int level)

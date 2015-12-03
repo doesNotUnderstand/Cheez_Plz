2fef271e96df9ad1227734909445dc548c7f7338
Leon Matsuki <lmatsuki87@gmail.com>
2015-12-02 23:05:37 -0800

    Added and finished Projectile room (sleeping cat non-functional)

--- Assets/_Scripts/StunDetector.cs
+++ Assets/_Scripts/StunDetector.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class StunDetector : MonoBehaviour {
 
+    public float stunTime = 2f;
+
 	IEnumerator OnTriggerEnter2D(Collider2D c)
 	{
 		float o_speed = c.gameObject.GetComponent<playerController> ().get_speed();
 		c.gameObject.GetComponent<playerController> ().set_speed (0);
-		yield return new WaitForSeconds (2f);
+		yield return new WaitForSeconds (stunTime);
 		c.gameObject.GetComponent<playerController> ().set_speed (o_speed);
 		Destroy (gameObject);
 	}
---
+++ Assets/_Scripts/cheeseBullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cheeseBullet : MonoBehaviour {
+
+    public playerController playerScript;
+
+	// Use this for initialization
+	void Start ()
+    {        
+        playerScript = GetComponent<Transform>().parent.gameObject.GetComponent<turret>().playerScript;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    { 
+        if (other.name == "Mouse")
+        {
+            playerScript.anim.SetBool("MouseFell", true);
+            playerScript.allowMovement(false);
+            playerScript.setPlayerDied(true);
+            Destroy(this.gameObject);
+        }
+        else if(other.tag == "DestroyBullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

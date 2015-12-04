52cb88cf02b64e8e05b4e7c23b64090ad8b79145
Carlos Aguirre <c.aguirre017@gmail.com>
2015-12-04 04:39:04 -0300

    MusicPuzzle & TelephonePuzzle Finish

--- Assets/_Scripts/StunDetector.cs
+++ Assets/_Scripts/StunDetector.cs
@@ -7,8 +7,8 @@ public class StunDetector : MonoBehaviour {
 
 	IEnumerator OnTriggerEnter2D(Collider2D c)
 	{
-		float o_speed = c.gameObject.GetComponent<playerController> ().get_speed();
-		c.gameObject.GetComponent<playerController> ().set_speed (0);
+		float o_speed = (float) c.gameObject.GetComponent<playerController>().get_speed();
+		c.gameObject.GetComponent<playerController>().set_speed (0);
 		yield return new WaitForSeconds (stunTime);
 		c.gameObject.GetComponent<playerController> ().set_speed (o_speed);
 		Destroy (gameObject);
